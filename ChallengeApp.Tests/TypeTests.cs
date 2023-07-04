5a4ccebf9412d21a41e7b64ea2ab4b6fa740dd74
Creidl <tannebaum.mariusz@gmail.com>
2023-07-04 23:10:02 +0200

    zadanie domowe na 8 dzień Testy typów danych

---
+++ ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,35 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void TestObjects()
+        {
+            Employee employee1 = new Employee("John");
+            Employee employee2 = new Employee("John");
+
+            Assert.AreNotEqual(employee1, employee2);
+        }
+        [Test]
+        public void TestInt()
+        {
+            int a= 11, b = 11;
+
+            Assert.AreEqual(a, b);
+        }
+        [Test]
+        public void TestFloat()
+        {
+            float a = 11.99f, b = 11.99f;
+
+            Assert.AreEqual(a, b);
+        }
+        [Test]
+        public void TestString()
+        {
+            string a = "11", b = "11";
+
+            Assert.AreEqual(a, b);
+        }
+    }
+}
