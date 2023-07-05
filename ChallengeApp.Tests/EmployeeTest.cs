16386092db306f145fa26d04978e3542540b1523
Creidl <tannebaum.mariusz@gmail.com>
2023-07-05 19:22:28 +0200

    Poprawione zadanie na dzień 9

--- ChallengeApp.Tests/EmployeeTest.cs
+++ ChallengeApp.Tests/EmployeeTest.cs
@@ -6,38 +6,44 @@ namespace ChallengeApp.Tests
         public void ScoreSumTest()
         {
             Employee employee1 = new Employee();
-            employee1.score.AddScore(1);
-            employee1.score.AddScore(2);
-            employee1.score.AddScore(3);
+            employee1.AddGrade(1);
+            employee1.AddGrade(2);
+            employee1.AddGrade(3);
+            Statistics statistics = employee1.GetStatistics();
 
-            Assert.AreEqual(6, employee1.score.GetScore());
+            Assert.AreEqual(6, statistics.Score);
         }
         [Test]
         public void MaxRatingTest()
         {
             Employee employee1 = new Employee();
-            employee1.score.AddScore(1);
-            employee1.score.AddScore(-2);
-            employee1.score.AddScore(-9);
-            Assert.AreEqual(1, employee1.score.Max);
+            employee1.AddGrade(1);
+            employee1.AddGrade(-2);
+            employee1.AddGrade(-9);
+            Statistics statistics = employee1.GetStatistics();
+            Assert.AreEqual(1, statistics.Max);
         }
         [Test]
         public void MinRatingTest()
         {
             Employee employee1 = new Employee();
-            employee1.score.AddScore(10);
-            employee1.score.AddScore(-2);
-            employee1.score.AddScore(-8);
-            Assert.That(employee1.score.Min, Is.EqualTo(-8));
+            employee1.AddGrade(10);
+            employee1.AddGrade(-2);
+            employee1.AddGrade(-8);
+            Statistics statistics = employee1.GetStatistics();
+
+            Assert.AreEqual(-8, statistics.Min);
         }
         [Test]
         public void AverageRatingTest()
         {
             Employee employee1 = new Employee();
-            employee1.score.AddScore(10);
-            employee1.score.AddScore(-2);
-            employee1.score.AddScore(-8);
-            Assert.That(employee1.score.Average, Is.EqualTo(0));
+            employee1.AddGrade(10);
+            employee1.AddGrade(-2);
+            employee1.AddGrade(-8);
+            Statistics statistics = employee1.GetStatistics();
+
+            Assert.AreEqual(0, statistics.Average);
         }
     }
 }
