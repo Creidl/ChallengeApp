bf279a0928f0ef3062aabcb1d4922bac93db3c8a
Creidl <tannebaum.mariusz@gmail.com>
2023-07-05 16:11:28 +0200

    zadanie na dzien 9

--- ChallengeApp.Tests/EmployeeTest.cs
+++ ChallengeApp.Tests/EmployeeTest.cs
@@ -3,32 +3,41 @@ namespace ChallengeApp.Tests
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void ScoreSumTest()
         {
             Employee employee1 = new Employee();
-            employee1.AddScore(1);
-            employee1.AddScore(2);
-            employee1.AddScore(3);
+            employee1.score.AddScore(1);
+            employee1.score.AddScore(2);
+            employee1.score.AddScore(3);
 
-            Assert.AreEqual(6, employee1.Score);
+            Assert.AreEqual(6, employee1.score.GetScore());
         }
         [Test]
-        public void Test2()
+        public void MaxRatingTest()
         {
             Employee employee1 = new Employee();
-            employee1.AddScore(1);
-            employee1.AddScore(-2);
-            employee1.AddScore(-9);
-            Assert.AreEqual(-10, employee1.Score);
+            employee1.score.AddScore(1);
+            employee1.score.AddScore(-2);
+            employee1.score.AddScore(-9);
+            Assert.AreEqual(1, employee1.score.Max);
         }
         [Test]
-        public void Test3()
+        public void MinRatingTest()
         {
             Employee employee1 = new Employee();
-            employee1.AddScore(10);
-            employee1.AddScore(-2);
-            employee1.AddScore(-8);
-            Assert.That(employee1.Score, Is.EqualTo(-0));
+            employee1.score.AddScore(10);
+            employee1.score.AddScore(-2);
+            employee1.score.AddScore(-8);
+            Assert.That(employee1.score.Min, Is.EqualTo(-8));
+        }
+        [Test]
+        public void AverageRatingTest()
+        {
+            Employee employee1 = new Employee();
+            employee1.score.AddScore(10);
+            employee1.score.AddScore(-2);
+            employee1.score.AddScore(-8);
+            Assert.That(employee1.score.Average, Is.EqualTo(0));
         }
     }
 }
