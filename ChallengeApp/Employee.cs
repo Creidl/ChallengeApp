810895c9789ec07eac01caa7a3f3087379f5073d
Creidl <92042198+Creidl@users.noreply.github.com>
2023-07-05 20:43:04 +0200

    Add files via upload

---
+++ ChallengeApp/Employee.cs
@@ -0,0 +1,36 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        private List<float> grade = new List<float>();
+        
+        public Employee(string firstName = "Zombie", string lastName = "X")
+        {
+            Name = firstName;
+            Surname = lastName;
+        }
+        public void AddGrade(float grade)
+        {
+            this.grade.Add(grade);
+        }
+        public Statistics GetStatistics()
+        {
+            Statistics stats = new Statistics();
+            stats.Max = float.MinValue;
+            stats.Min = float.MaxValue;
+            stats.Average = 0;
+
+            foreach (var grade in this.grade)
+            {
+                stats.Average += grade;
+                stats.Max = Math.Max(grade,stats.Max);
+                stats.Min = Math.Min(grade,stats.Min);
+            }
+            stats.Average /= this.grade.Count;
+            stats.Score = grade.Sum();
+            return stats;
+        }
+    }
+}
---
+++ ChallengeApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using ChallengeApp;
+
+Employee user1 = new Employee("Sam", "Serious");
+
+user1.AddGrade(1);
+user1.AddGrade(2);
+user1.AddGrade(3);
+
+Statistics statistics = user1.GetStatistics();
+
+Console.WriteLine($"imię: {user1.Name} nazwisko: {user1.Surname} ");
+Console.WriteLine($"Maksymalna ocena: {statistics.Max} Minimalna ocena: {statistics.Min} Średnia ocen: {statistics.Average:N2}");
