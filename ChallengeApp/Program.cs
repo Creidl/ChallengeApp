1f64ee4b6029920dd08bab81c535295d61520cd9
Creidl <tannebaum.mariusz@gmail.com>
2023-07-06 23:16:10 +0200

    Przygotowanie do 12 dnia

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -6,14 +6,7 @@ user1.AddGrade(2.09f);
 user1.AddGrade(3.33);
 user1.AddGrade("12");
 
-List<Statistics> statistics = new List<Statistics>();
-statistics.Add(user1.GetStatistics()); // tu zastosowano foreach
-statistics.Add(user1.GetStatisticsWithFor());
-statistics.Add(user1.GetStatisticsWithDoWhile());
-statistics.Add(user1.GetStatisticsWithWhile());
+Statistics statistics = user1.GetStatistics(); 
 
 Console.WriteLine($"\n\timię: {user1.Name}\n\tnazwisko: {user1.Surname} ");
-foreach(Statistics stat in statistics)
-{
-    Console.WriteLine($"\nMaksymalna ocena: {stat.Max}; Minimalna ocena: {stat.Min}; Średnia ocen: {stat.Average:N2}");
-}
+Console.WriteLine($"\nMaksymalna ocena: {statistics.Max}; Minimalna ocena: {statistics.Min}; Średnia ocen: {statistics.Average:N2}");
